6fb4065f77d56b65457e89286c969a759edde56e
al3p <al3.pirovano@gmail.com>
2024-08-13 19:26:54 +0200

    working copy. every 10s (fast timer) the event status cycles.

--- QAction_90/QAction_90.cs
+++ QAction_90/QAction_90.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Skyline.DataMiner.Scripting;
 using Skyline.Protocol.MyExtension;
+using SLNetMessages = Skyline.DataMiner.Net.Messages;
 
 /// <summary>
 /// DataMiner QAction Class: QAStateCycle.
@@ -21,23 +22,20 @@ public static class QAction
         {
             protocol.Log($"QA{protocol.QActionID}|{protocol.GetTriggerParameter()}|Run|Executing QAction", LogType.DebugInfo, LogLevel.NoLogging);
 
-            /*
-            TO BE DONE. setting a column is too much of a burden
             uint[] columnsIdx = new uint[] { 0, 2 };
             object[] columns = MyCommons.GetColumns(protocol, protocol.tbleventsoverview.TableId, columnsIdx);
             object[] keyCol = (object[])columns[0];
             object[] valueCol = (object[])columns[1];
             string[] strKeyCol = new string[keyCol.Length];
-
-
+ 
             for (uint i = 0; i < valueCol.Length; i++)
             {
-                ///valueCol[i] = (object)((((int)(valueCol[i])) + 1) % 4);
-                strKeyCol[i] = (string)keyCol[i];
-            }
+                valueCol[i] = (((Convert.ToUInt32(valueCol[i])) + 1) % 4);
+                strKeyCol[i] = Convert.ToString(keyCol[i]);
+             }
 
-            protocol.tbleventsoverview.SetColumn(2, strKeyCol, valueCol, false);
-            */
+            protocol.tbleventsoverview.SetColumn(Parameter.Tbleventsoverview.Pid.coleventsoverviewstatus_103, strKeyCol, valueCol, false);
+           
 
         }
         catch (Exception ex)
